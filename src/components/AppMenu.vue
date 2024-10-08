6fe6a242a9b3ec2185969a072cbabc5da4e77365
sanbei101 <sanbei101@outlook.com>
2024-10-08 19:55:40 +0800

    Refactor App.vue component structure

---
+++ src/components/AppMenu.vue
@@ -0,0 +1,19 @@
+<template>
+    <n-menu :options="menuOptions" mode="vertical" />
+</template>
+
+
+<script setup lang="ts">
+import { type MenuOption } from 'naive-ui'
+import { HomeOutline, SettingsOutline } from '@vicons/ionicons5'; // 导入图标
+const menuOptions: MenuOption[] = [
+    {
+        label: () => h(HomeOutline), // 使用图标替代文字
+        key: 'collection'
+    },
+    {
+        label: () => h(SettingsOutline), // 使用设置图标替代文字
+        key: 'setting'
+    }
+];
+</script>
---
+++ src/components/AppSubMenu.vue
@@ -0,0 +1,20 @@
+<template>
+    <div style="display: flex; flex-direction: column; align-items: center; gap: 1rem">
+        <n-input placeholder="search collections.." style="margin: 1rem auto; max-width: 85%" />
+        <div v-for="collection in collections" :key="collection">
+            <n-button strong secondary type="tertiary" style="min-width: 80%">
+                <template #icon>
+                    <n-icon>
+                        <Cash />
+                    </n-icon>
+                </template>
+                {{ collection }}
+            </n-button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Cash } from '@vicons/ionicons5'
+const collections = ref<string[]>(['Collection', 'User']);
+</script>
