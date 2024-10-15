1889695f5d6b6182533716a9f42601fb76bee547
Sanbei <sanbei101@outlook.com>
2024-10-15 23:31:09 +0800

    Update axios.ts

--- src/util/axios.ts
+++ src/util/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:3000',
+  baseURL: 'http://62.234.178.169:3000',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
