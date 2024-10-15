9d96f770253b572801ffa96b789fd1c82d752310
sanbei101 <sanbei101@outlook.com>
2024-10-15 23:01:56 +0800

    Refactor imports and update API base URL

--- src/util/axios.ts
+++ src/util/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-  baseURL: 'https://fc.sanbei101.tech/pock',
+  baseURL: 'http://127.0.0.1:3000',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
